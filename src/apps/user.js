d9f0e6388410503b9e61423bf19202af4015a04f
Bunny <99755679+402-RUGER@users.noreply.github.com>
2023-04-24 23:40:45 +0700

    Add

--- src/apps/user.js
+++ src/apps/user.js
@@ -26,3 +26,110 @@ export function User(props) {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+/*import { useState } from "react";
+
+export function User(props) {
+    /*let user = {
+        name: 'John',
+        last_name: 'Barlowe',
+        age: 30,
+        cat: {
+            name: 'Happy',
+            age: 1,
+        },
+        salaries: [
+           {
+               salary: '5670',
+               from_date: '2019'
+           },
+           {
+               salary: '1670',
+               from_date: '2018'
+           },
+           {
+               salary: '15670',
+               from_date: '2012'
+           },
+           ],
+        };*/
+
+        /*console.log(props);
+
+        const [objArr, setvalue] = useState(User);
+
+        const result = objArr.map((obj) => {
+            return <p key={obj.salaries}>
+                {obj.salary} {obj.from_date}
+            </p>
+        });
+
+        return <div>
+           {result}
+        </div>;
+    
+ 
+
+}*/
+
+
+
+
+
+
+
+//import { useState } from "react";  
+
+//exsport 
+
+
+//export function Calculator(props) {
+    /*const [text, setText] = useState('');
+
+    //let text = '';
+
+    //console.log(text);
+    console.log(props);
+
+    return <> 
+    <p>{text}</p>
+    <input type= 'text' placeholder="Text" onChange={(event) => {
+       // console.log(event.target.value);
+       // text = event.target.value;
+       // test
+       setText(event.target.value);
+    }}/>
+    </>;*/
+//}
+    //export function User(props) {
+        
+       // let User = [];
+
+       // console.log(props);
+        
+    //}
+    
+/*class user extends Component {
+    render() {
+        console.log('items', this.props.items);
+        return (
+            <div>
+                {this.props.item.map((item, index) => 
+                <a href={item.})}
+                </div>
+             );
+
+        }
+    }*/
+
+
